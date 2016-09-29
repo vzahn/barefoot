05f6add6aaf3b2bef1c356b928462598be9f4597
Volker Zahn <volker.zahn@t-systems.com>
2016-09-29 14:26:58 +0200

    GPS-Tracks in Bonn added and GPX to JSON converter added

---
+++ util/xml2json/run.sh
@@ -0,0 +1,2 @@
+#java -cp ".;jdom-2.0.6.jar;json-20140107.jar" xml2json.Converter "2016-09-11 31km Run.gpx" -i id1
+java -cp ".:jdom-2.0.6.jar:json-20140107.jar" xml2json.Converter "2016-09-11 31km Run.gpx" -i id1
